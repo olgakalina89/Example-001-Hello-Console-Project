3c464db6bc458dc34cce54a6c47d0ca0359b580f
Olga <mrs.olgakalina@gmail.com>
2022-09-03 13:25:33 +0300

    Add Example 014 Recursion Algorithm

---
+++ Example014_Recursion_Algorithm/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+int n = 1;
+void FindWords(string alphabet, char[] word, int length = 0)
+{
+    if (length == word.Length)
+    {
+        WriteLine($"{n++} {new String(word)}"); return;
+    }
+    for (int i = 0; i < alphabet.Length; i++)
+    {
+        word[length] = alphabet[i];
+        FindWords(alphabet, word, length + 1);
+    }
+}
+FindWords("аисв", new char[2]);
