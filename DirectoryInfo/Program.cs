9c4fc784253fbbfd902629fc6e12d2925aedb846
Olga <mrs.olgakalina@gmail.com>
2022-09-03 13:55:32 +0300

    added new info

--- DirectoryInfo/Program.cs
+++ DirectoryInfo/Program.cs
@@ -2,3 +2,9 @@
 string path = "C:\Users\Оля\OneDrive\Рабочий стол\C#\Examples\Example001_HelloConsole";
 DirectoryInfo di = new DirectoryInfo(path);
 WriteLine(di.CreationTime);
+FileInfo[] fi = di.GetFiles();
+
+for (int i = 0; i < fi.Length; i++)
+{
+    WriteLine(fi[i].Name);
+}
