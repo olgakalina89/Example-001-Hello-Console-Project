fdd3f031e36c941cb4134e20ed287f80641f7e87
Olga <mrs.olgakalina@gmail.com>
2022-09-03 13:35:08 +0300

    Add Directory Info

---
+++ DirectoryInfo/Program.cs
@@ -0,0 +1,4 @@
+﻿using static System.Console;
+string path = "C:\Users\Оля\OneDrive\Рабочий стол\C#\Examples\Example001_HelloConsole";
+DirectoryInfo di = new DirectoryInfo(path);
+WriteLine(di.CreationTime);
