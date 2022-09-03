8be5516b75d57d02d40ca0618716a55b5aa85158
Olga <mrs.olgakalina@gmail.com>
2022-09-03 14:09:32 +0300

    Add Catalog Info

---
+++ CatalogInfo/Program.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+Clear();
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        WriteLine($"{indent}{files[i].Name}");
+    }
+}
+
+string path = @"\Users\Оля\OneDrive\Рабочий стол\VERSIONS CONTROL";
+CatalogInfo(path);
